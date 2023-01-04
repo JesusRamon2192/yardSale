a4ea079461665b67ab9e6be414f04a3ae0d833e6
jesusRamon <jesus.ramon2192@gmail.com>
2023-01-04 13:14:18 -0600

    Se le agregan algunos estilos a la caja del menuEmail

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
+const $ = (selector) => document.querySelector(selector);
+
+const menuEmail = $(".navbar-email");
+const desktopMenu = $(".desktop-menu");
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 
